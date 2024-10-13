7b362acf53111b1fed4206b5edb3e23cc5bab776
Damindu Prasadith <adaminduprasadith@gmail.com>
2024-10-13 22:20:57 +0530

    tut2

---
+++ src/tut2_question1.java
@@ -0,0 +1,23 @@
+public class tut2_question1 {
+    public static void main(String[] args) {
+
+        for(int i =10;i<50;i++){
+
+            if(i%10==0){
+                System.out.println(" ");
+                System.out.print(i +" ");
+
+            }
+            else {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+
+
+
+
+        }
+
+
+    }
+}
